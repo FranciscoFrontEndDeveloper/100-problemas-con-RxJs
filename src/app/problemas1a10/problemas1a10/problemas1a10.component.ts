236387a19148b554b26984cfbd80e61bf28c21b7
FranciscoFrontEndDeveloper <pachonetworking@gmail.com>
2021-12-30 02:31:06 -0500

    avance problema 1

--- src/app/problemas1a10/problemas1a10/problemas1a10.component.ts
+++ src/app/problemas1a10/problemas1a10/problemas1a10.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable, fromEvent } from 'rxjs';
 @Component({
   selector: 'app-problemas1a10',
   templateUrl: './problemas1a10.component.html',
-  styleUrls: ['./problemas1a10.component.sass']
+  styleUrls: ['./problemas1a10.component.sass'],
 })
 export class Problemas1a10Component implements OnInit {
+  public sumarDosNumeros: any;
+  public NumbSum1: any;
+  public NumbSum2: any;
+  public botonSumar: any;
+  public ResultadoSuma: any;
+  constructor() {
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.sumarDosNumbs();
+  }
+
+  sumarDosNumbs(): any {
+    this.botonSumar = document.querySelector('#sumar');
+    const SumaDosNumeros = fromEvent(this.botonSumar, 'click');
+    SumaDosNumeros.subscribe(() => {
+      this.NumbSum1 = document.querySelector('#sumNumb1');
+      this.NumbSum2 = document.querySelector('#sumNumb2');
+      this.ResultadoSuma = this.NumbSum1.value += this.NumbSum2.value;
+    });
+  }
 }
