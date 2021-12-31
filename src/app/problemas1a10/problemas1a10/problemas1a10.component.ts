89fcde3a89b4628d4bb1fddf6dd0890cbe393368
FranciscoFrontEndDeveloper <pachonetworking@gmail.com>
2021-12-31 01:56:00 -0500

    suma funcional

--- src/app/problemas1a10/problemas1a10/problemas1a10.component.ts
+++ src/app/problemas1a10/problemas1a10/problemas1a10.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, fromEvent } from 'rxjs';
+import { Observable, fromEvent, from } from 'rxjs';
 @Component({
   selector: 'app-problemas1a10',
   templateUrl: './problemas1a10.component.html',
@@ -11,12 +11,22 @@ export class Problemas1a10Component implements OnInit {
   public NumbSum2: any;
   public botonSumar: any;
   public ResultadoSuma: any;
+  public array2: any;
+  // public observer: any;
   constructor() {
-
+    this.array2  = ['One', 'Two', 'Three'];
   }
 
   ngOnInit(): void {
     this.sumarDosNumbs();
+    const observer = {
+      next: (x: any) => console.log('observador  con next ' + x),
+      error: (err: any) => console.log('observador con error' + err),
+      complete: () => console.log('observador completado'),
+    };
+
+    fromEvent(document, 'click');
+    from(this.array2).subscribe(observer);
   }
 
   sumarDosNumbs(): any {
@@ -25,7 +35,12 @@ export class Problemas1a10Component implements OnInit {
     SumaDosNumeros.subscribe(() => {
       this.NumbSum1 = document.querySelector('#sumNumb1');
       this.NumbSum2 = document.querySelector('#sumNumb2');
-      this.ResultadoSuma = this.NumbSum1.value += this.NumbSum2.value;
+      this.ResultadoSuma =
+        parseInt( this.NumbSum1.value) + parseInt(this.NumbSum2.value);
     });
   }
+
+
+
+
 }
