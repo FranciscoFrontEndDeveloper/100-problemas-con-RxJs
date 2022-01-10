bd4c39e876251f12c966a602260e7aa55b06edde
FranciscoFrontEndDeveloper <pachonetworking@gmail.com>
2022-01-10 02:01:00 -0500

    problema dos resuelto

--- src/app/problemas1a10/problemas1a10/problemas1a10.component.ts
+++ src/app/problemas1a10/problemas1a10/problemas1a10.component.ts
@@ -9,38 +9,48 @@ export class Problemas1a10Component implements OnInit {
   public sumarDosNumeros: any;
   public NumbSum1: any;
   public NumbSum2: any;
+  public NumbDiv1: any;
+  public NumbDiv2: any;
   public botonSumar: any;
   public ResultadoSuma: any;
+  public ResultadoDivision: any;
   public array2: any;
+  public botonDividir: any;
   // public observer: any;
   constructor() {
-    this.array2  = ['One', 'Two', 'Three'];
+    this.array2 = ['One', 'Two', 'Three'];
   }
 
   ngOnInit(): void {
     this.sumarDosNumbs();
-    const observer = {
-      next: (x: any) => console.log('observador  con next ' + x),
-      error: (err: any) => console.log('observador con error' + err),
-      complete: () => console.log('observador completado'),
-    };
+    this.residuoDosNumeros();
 
     fromEvent(document, 'click');
-    from(this.array2).subscribe(observer);
+    // from(this.array2).subscribe(observer);
   }
 
   sumarDosNumbs(): any {
     this.botonSumar = document.querySelector('#sumar');
     const SumaDosNumeros = fromEvent(this.botonSumar, 'click');
-    SumaDosNumeros.subscribe(() => {
-      this.NumbSum1 = document.querySelector('#sumNumb1');
-      this.NumbSum2 = document.querySelector('#sumNumb2');
-      this.ResultadoSuma =
-        parseInt( this.NumbSum1.value) + parseInt(this.NumbSum2.value);
-    });
+    const observerSuma = {
+      next: () => {
+        this.NumbSum1 = document.querySelector('#sumNumb1');
+        this.NumbSum2 = document.querySelector('#sumNumb2');
+        this.ResultadoSuma =
+          parseInt(this.NumbSum1.value) + parseInt(this.NumbSum2.value);
+      },
+    };
+    SumaDosNumeros.subscribe(observerSuma);
   }
 
-
-
-
+  residuoDosNumeros(): any {
+    this.botonDividir = document.querySelector('#dividir');
+    const ResiduoDosNumeros = fromEvent(this.botonDividir, 'click');
+    ResiduoDosNumeros.subscribe(() => {
+      this.NumbDiv1 = document.querySelector('#divNumb1');
+      this.NumbDiv2 = document.querySelector('#divNumb2');
+      this.ResultadoDivision =
+        parseInt(this.NumbDiv1.value) / parseInt(this.NumbDiv2.value);
+    });
+  }
 }
